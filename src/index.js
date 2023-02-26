bd043ecbb01f5e7911f375d4c76feb2c381cf8c9
kxs6629 <35939549+kxs6629@users.noreply.github.com>
2023-02-26 00:16:11 -0500

    Can now read through json file and create markers

--- src/index.js
+++ src/index.js
@@ -2,32 +2,27 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicmRsNDE5OSIsImEiOiJja3poYTVwamwzenhtMnVuazZ2Z
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    center: [-77.675, 43.084], // starting position [lng, lat]
+    zoom: 15, // starting zoom
 });
 
-async function readLocationData(){
-    // WAIT
-}
-const file = "../resources/markers.json"
-var a = fetch('../resources/markers.json')
-    .then((response) => response.json())
-    .then((json) => json.locations);
-for (const key in a){
-    console.log(key);
-    const marker = new mapboxgl.Marker()
-    .setLngLat(key.lng,key.lat)
-    .setPopup(new mapboxgl.Popup().setHTML("<h1>"+key.name+"</h1>"))
-    .addTo(map)      
-}
-
-map.on('click',(e) => {
-    console.log(e)
-    const marker = new mapboxgl.Marker({
+fetch("../resources/markers.json")
+    .then(response => response.json())
+    .then(data => data.locations.forEach(element => {
+        const marker = new mapboxgl.Marker()
+        .setLngLat(new mapboxgl.LngLat(element.lng,element.lat))
+        .setPopup(new mapboxgl.Popup().setHTML("<p>"+element.name+"</p>"))
+        .addTo(map)      
+        
+    }));
+// Commented this section out, but it can still serve as an example piece for onclick events
+// map.on('click',(e) => {
+    // console.log(e)
+    // const marker = new mapboxgl.Marker({
         // draggable:true
-    })
-        .setLngLat([e.lngLat.lng,e.lngLat.lat])
-        .setPopup(new mapboxgl.Popup().setHTML("<h1>Test Marker!</h1>"))
-        .addTo(map);
+    // })
+        // .setLngLat([e.lngLat.lng,e.lngLat.lat])
+        // .setPopup(new mapboxgl.Popup().setHTML("<h1>Test Marker!</h1>"))
+        // .addTo(map);
     // console.log(e.lngLat)
-});
+// });
